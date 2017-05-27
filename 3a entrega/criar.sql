b82a6546775c04912ad376368b57a8fd5df4aaf1
IgorSilveira00 <up201505172@fe.up.pt>
2017-05-27 19:19:30 +0100

    merge

--- 3a entrega/criar.sql
+++ 3a entrega/criar.sql
@@ -9,8 +9,10 @@ create table Utilizador (
   telefone VARCHAR(255) UNIQUE,
   password VARCHAR(18) NOT NULL,
   morada VARCHAR(255) DEFAULT 'N/D',
-  paisID BLOB,
-  FOREIGN KEY (paisID) REFERENCES Pais(paisID)
+  paisID INTEGER,
+  codigoPostal CHAR(4) DEFAULT '0000',
+  FOREIGN KEY (paisID) REFERENCES Pais(paisID) ON DELETE SET NULL ON UPDATE CASCADE,
+  FOREIGN KEY (codigoPostal) REFERENCES Localidade(codigoPostal) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 drop table if exists UtilizadorSegueUtilizador;
@@ -18,83 +20,74 @@ create table UtilizadorSegueUtilizador (
   username1 VARCHAR(20),
   username2 VARCHAR(20),
   PRIMARY KEY (username1, username2),
-  FOREIGN KEY (username1) REFERENCES Utilizador(username),
-  FOREIGN KEY (username2) REFERENCES Utilizador(username)
+  FOREIGN KEY (username1) REFERENCES Utilizador(username) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (username2) REFERENCES Utilizador(username) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 drop table if exists ArtistaRelacionaArtista;
 create table ArtistaRelacionaArtista (
-  aID1 BLOB,
-  aID2 BLOB,
-  PRIMARY KEY (aID1, aID2),
-  FOREIGN KEY (aID1) REFERENCES Artista(aID),
-  FOREIGN KEY (aID2) REFERENCES Artista(aID)
-);
-
-drop table if exists UtilizadorLocaliza;
-create table UtilizadorLocaliza (
-  username VARCHAR(20),
-  cpGeral VARCHAR(20),
-  PRIMARY KEY (username, cpGeral),
-  FOREIGN KEY (username) REFERENCES Utilizador(username),
-  FOREIGN KEY (cpGeral) REFERENCES Localidade(cpGeral)
+  artistaID1 INTEGER,
+  artistaID2 INTEGER,
+  PRIMARY KEY (artistaID1, artistaID2),
+  FOREIGN KEY (artistaID1) REFERENCES Artista(artistaID) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (artistaID2) REFERENCES Artista(artistaID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 drop table if exists UtilizadorGuardaPlaylist;
 create table UtilizadorGuardaPlaylist (
   username VARCHAR(20),
-  plID INT,
-  PRIMARY KEY (username, plID),
-  FOREIGN KEY (username) REFERENCES Utilizador(username),
-  FOREIGN KEY (plID) REFERENCES Playlist(plID)
+  playlistID INTEGER,
+  PRIMARY KEY (username, playlistID),
+  FOREIGN KEY (username) REFERENCES Utilizador(username) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (playlistID) REFERENCES Playlist(playlistID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 drop table if exists UtilizadorFavoritaMusica;
 create table UtilizadorFavoritaMusica (
   username VARCHAR(20),
-  muID INT,
-  PRIMARY KEY (username, muID),
-  FOREIGN KEY (username) REFERENCES Utilizador(username),
-  FOREIGN KEY (muID) REFERENCES Musica(muID)
+  musicaID INTEGER,
+  PRIMARY KEY (username, musicaID),
+  FOREIGN KEY (username) REFERENCES Utilizador(username) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (musicaID) REFERENCES Musica(musicaID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 drop table if exists UtilizadorOuveMusica;
 create table UtilizadorOuveMusica (
   username VARCHAR(20),
-  muID INT,
-  time_stamp VARCHAR(255) NOT NULL,
-  PRIMARY KEY (username, muID, time_stamp),
-  FOREIGN KEY (username) REFERENCES Utilizador(username),
-  FOREIGN KEY (muID) REFERENCES Musica(muID)
+  musicaID INTEGER,
+  time_stamp DATE NOT NULL,
+  PRIMARY KEY (username, musicaID, time_stamp),
+  FOREIGN KEY (username) REFERENCES Utilizador(username) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (musicaID) REFERENCES Musica(musicaID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 drop table if exists UtilizadorFavoritaAlbum;
 create table UtilizadorFavoritaAlbum (
   username VARCHAR(20),
-  abID INT,
-  PRIMARY KEY (username, abID),
-  FOREIGN KEY (username) REFERENCES Utilizador(username),
-  FOREIGN KEY (abID) REFERENCES Album(abID)
+  albumID INTEGER,
+  PRIMARY KEY (username, albumID),
+  FOREIGN KEY (username) REFERENCES Utilizador(username) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (albumID) REFERENCES Album(albumID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 drop table if exists UtilizadorSegueArtista;
 create table UtilizadorSegueArtista (
   username VARCHAR(20),
-  aID BLOB,
-  PRIMARY KEY (username, aID),
-  FOREIGN KEY (username) REFERENCES Utilizador(username),
-  FOREIGN KEY (aID) REFERENCES Artista(aID)
+  artistaID INTEGER,
+  PRIMARY KEY (username, artistaID),
+  FOREIGN KEY (username) REFERENCES Utilizador(username) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (artistaID) REFERENCES Artista(artistaID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 drop table if exists Conta;
 create table Conta (
-  cID BLOB PRIMARY KEY,
-  preco FLOAT NOT NULL CHECK(preco>=0),
+  contaID INTEGER PRIMARY KEY AUTOINCREMENT,
+  preco FLOAT NOT NULL CHECK (preco>=0.00),
   dataInicio DATE NOT NULL,
-  dataFim DATE NOT NULL CHECK(dataInicio<dataFim),
-  utilizador VARCHAR(20) NOT NULL REFERENCES Utilizador(username),
-  pagamento VARCHAR(13) NOT NULL REFERENCES pagamento CHECK(pagamento in ('gratuito', 'CartaoCredito', 'Paypal', 'PaysafeCard')),
-  tipoConta VARCHAR(8) NOT NULL REFERENCES TipoConta CHECK(tipoConta in ('0.0', '6.99', '10.99'))
+  dataFim DATE CHECK (dataFim > dataInicio) DEFAULT 'N/D',
+  username VARCHAR(20) NOT NULL REFERENCES Utilizador(username) ON DELETE CASCADE ON UPDATE CASCADE,
+  pagamento VARCHAR(13) NOT NULL REFERENCES pagamento ON DELETE CASCADE ON UPDATE CASCADE CHECK(pagamento in ('gratuito', 'CartaoCredito', 'Paypal', 'PaysafeCard')),
+  tipoConta FLOAT NOT NULL REFERENCES TipoConta ON DELETE CASCADE ON UPDATE CASCADE CHECK(tipoConta in (0.00, 6.99))
 );
 
 drop table if exists Pagamento;
@@ -104,119 +97,117 @@ create table Pagamento (
 
 drop table if exists TipoConta;
 create table TipoConta (
-  precoMensal VARCHAR(8) PRIMARY KEY CHECK(precoMensal in ('0.0', '6.99', '10.99'))
+  precoMensal FLOAT PRIMARY KEY CHECK(precoMensal in (0.00, 6.99))
 );
 
 drop table if exists Localidade;
 create table Localidade (
-  cpGeral CHAR(4) PRIMARY KEY,
-  nome VARCHAR(255) NOT NULL,
-  paisID INT NOT NULL CHECK(paisID > 0) REFERENCES Pais,
-  FOREIGN KEY (paisID) REFERENCES Pais(paisID)
+  codigoPostal CHAR(4) PRIMARY KEY,
+  nome VARCHAR(255) NOT NULL
 );
 
 drop table if exists Playlist;
 create table Playlist (
-  plID BLOB PRIMARY KEY,
-  nome VARCHAR(255) DEFAULT 'plID',
+  playlistID INTEGER PRIMARY KEY AUTOINCREMENT,
+  nome VARCHAR(255) DEFAULT 'playme',
   criador VARCHAR(20),
-  FOREIGN KEY (criador) REFERENCES Utilizador(username)
+  FOREIGN KEY (criador) REFERENCES Utilizador(username) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 drop table if exists PlaylistGuardaMusica;
 create table PlaylistGuardaMusica (
-  plID BLOB,
-  muID BLOB,
-  PRIMARY KEY (plID, muID),
-  FOREIGN KEY (plID) REFERENCES Playlist(plID),
-  FOREIGN KEY (muID) REFERENCES Musica(muID)
+  playlistID INTEGER,
+  musicaID INTEGER,
+  PRIMARY KEY (playlistID, musicaID),
+  FOREIGN KEY (playlistID) REFERENCES Playlist(playlistID) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (musicaID) REFERENCES Musica(musicaID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 drop table if exists Musica;
 create table Musica (
-  muID BLOB PRIMARY KEY,
+  musicaID INTEGER PRIMARY KEY AUTOINCREMENT,
   nome VARCHAR(255) NOT NULL,
   duracao INT NOT NULL CHECK(duracao>0)
 );
 
 drop table if exists Album;
 create table Album (
-  abID BLOB PRIMARY KEY,
+  albumID INTEGER PRIMARY KEY AUTOINCREMENT,
   nome VARCHAR(255) NOT NULL,
   ano INT NOT NULL CHECK(Ano>1900),
-  tipoID INT NOT NULL CHECK(tipoID > 0) REFERENCES Tipo(tipoID),
-  FOREIGN KEY (tipoID) REFERENCES Tipo(tipoID)
+  tipoAlbumID INT NOT NULL CHECK(tipoAlbumID > 0),
+  FOREIGN KEY (tipoAlbumID) REFERENCES Tipo(tipoAlbumID) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 drop table if exists AlbumRelacionaAlbum;
 create table AlbumRelacionaAlbum (
-  abID1 BLOB,
-  abID2 BLOB,
-  PRIMARY KEY (abID1, abID2),
-  FOREIGN KEY (abID1) REFERENCES Album(abID),
-  FOREIGN KEY (abID2) REFERENCES Album(abID)
+  albumID1 INTEGER,
+  albumID2 INTEGER,
+  PRIMARY KEY (albumID1, albumID2),
+  FOREIGN KEY (albumID1) REFERENCES Album(albumID) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (albumID2) REFERENCES Album(albumID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-drop table if exists AlbumGenero;
-create table AlbumGenero (
-  abID BLOB,
-  genID BLOB,
-  PRIMARY KEY (abID, genID),
-  FOREIGN KEY (abID) REFERENCES Album(abID),
-  FOREIGN KEY (genID) REFERENCES Genero(genID)
+drop table if exists AlbumGeneroMusical;
+create table AlbumGeneroMusical (
+  albumID INTEGER,
+  generoMusicalID INTEGER,
+  PRIMARY KEY (albumID, generoMusicalID),
+  FOREIGN KEY (albumID) REFERENCES Album(albumID) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (generoMusicalID) REFERENCES GeneroMusical(generoMusicalID) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 drop table if exists Artista;
 create table Artista (
-  aID BLOB PRIMARY KEY,
+  artistaID INTEGER PRIMARY KEY AUTOINCREMENT,
   nome VARCHAR(255) NOT NULL,
   biografia VARCHAR(255) DEFAULT 'Sem Informacao',
-  paisID BLOB NOT NULL CHECK(paisID>0) REFERENCES pais(paisID),
-  FOREIGN KEY (paisID) REFERENCES Pais(paisID)
+  paisID INTEGER NOT NULL CHECK(paisID>0),
+  FOREIGN KEY (paisID) REFERENCES Pais(paisID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 drop table if exists MusicaAlbumArtista;
 create table MusicaAlbumArtista (
-  muID BLOB,
-  abID BLOB,
-  aID BLOB,
-  PRIMARY KEY (muID, abID, aID),
-  FOREIGN KEY (aID) REFERENCES Artista(aID),
-  FOREIGN KEY (abID) REFERENCES Album(abID),
-  FOREIGN KEY (muID) REFERENCES Musica(muID)
+  musicaID INTEGER,
+  albumID INTEGER,
+  artistaID INTEGER,
+  PRIMARY KEY (musicaID, albumID, artistaID),
+  FOREIGN KEY (artistaID) REFERENCES Artista(artistaID) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (albumID) REFERENCES Album(albumID) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (musicaID) REFERENCES Musica(musicaID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 drop table if exists Publicitario;
 create table Publicitario (
-  pID BLOB PRIMARY KEY,
+  publicitarioID INTEGER PRIMARY KEY AUTOINCREMENT,
   nome VARCHAR(255) NOT NULL,
   investimento INT NOT NULL CHECK(investimento>0),
-  paisID BLOB NOT NULL CHECK(paisID>0) REFERENCES Pais(paisID),
-  FOREIGN KEY (paisID) REFERENCES Pais(paisID)
+  paisID INTEGER NOT NULL CHECK(paisID>0),
+  FOREIGN KEY (paisID) REFERENCES Pais(paisID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 drop table if exists Publicidade;
 create table Publicidade (
-  pbID BLOB PRIMARY KEY,
+  publicidadeID INTEGER PRIMARY KEY AUTOINCREMENT,
   duracao INTEGER NOT NULL CHECK(duracao>0),
-  pID BLOB NOT NULL,
-  FOREIGN KEY (pID) REFERENCES Publicitario(pID)
+  publicitarioID INTEGER NOT NULL,
+  FOREIGN KEY (publicitarioID) REFERENCES Publicitario(publicitarioID) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 drop table if exists Pais;
 create table Pais (
-  paisID BLOB PRIMARY KEY,
+  paisID INTEGER PRIMARY KEY AUTOINCREMENT,
   nome VARCHAR(255) UNIQUE
 );
 
 drop table if exists Tipo;
 create table Tipo (
-  tipoID BLOB PRIMARY KEY,
+  tipoAlbumID INTEGER PRIMARY KEY AUTOINCREMENT,
   nome VARCHAR(255) UNIQUE
 );
 
-drop table if exists Genero;
-create table Genero (
-  genID BLOB PRIMARY KEY,
+drop table if exists GeneroMusical;
+create table GeneroMusical (
+  generoMusicalID INTEGER PRIMARY KEY AUTOINCREMENT,
   nome VARCHAR(255) UNIQUE
 );
