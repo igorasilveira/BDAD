ac6514b12731b73e940b861badb5ca133e73bb0e
BVFernandes <up200707284@fe.up.pt>
2017-05-28 22:17:56 +0100

    Update Q10

--- 3a entrega/query10.sql
+++ 3a entrega/query10.sql
@@ -2,6 +2,11 @@
 .headers ON
 .nullvalue NULL
 
--- listar musicas que aparecamm em mais de 3 albuns
+-- listar musicas que aparecamm em mais de um album
 
-SELECT Musica.nome AS Musica
+SELECT Musica.nome, COUNT(Album.albumID) AS numMusicasVariosAlbuns
+FROM MusicaAlbumArtista
+INNER JOIN Musica ON Musica.musicaID=MusicaAlbumArtista.musicaID
+INNER JOIN Album ON Album.albumID=MusicaAlbumArtista.albumID
+GROUP BY Musica.nome
+HAVING numMusicasVariosAlbuns> 1
