8972f4eaa0c5a03f16875ad70f376f1b0613e54e
JMendes25 <up201505439@fe.up.pt>
2017-05-28 23:15:03 +0100

    8 done. missing 4,5,6,9

--- 3a entrega/int8.sql
+++ 3a entrega/int8.sql
@@ -4,42 +4,20 @@
 .headers ON
 .nullvalue NULL
 
-SELECT Artista.nome AS Artista
-FROM Artista
+
+SELECT Artista.nome AS Artista, Utilizador.username AS Utilizador
+FROM Artista, Utilizador
 WHERE Artista.artistaID in (
   SELECT M.artistaID
   FROM UtilizadorOuveMusica U, MusicaAlbumArtista M
   WHERE U.musicaID = M.musicaID
           AND
-        U.username = 'sfisher18'
+        U.username = Utilizador.username
 )
 AND Artista.artistaID not in (
   SELECT U.artistaID
   FROM UtilizadorSegueArtista U
-  WHERE U.username = 'sfisher18'
+  WHERE U.username = Utilizador.username
 )
+ORDER BY Artista
 ;
-
---CREATE VIEW MusicaOuvida AS
---SELECT DISTINCT Musica.musicaID AS musicaID
---FROM Musica, Utilizador JOIN UtilizadorOuveMusica
---ON Utilizador.username = UtilizadorOuveMusica.username AND Musica.musicaID = UtilizadorOuveMusica.musicaID
---WHERE Utilizador.username = 'sfisher18'
---GROUP BY Musica.nome;
-
-
---SELECT DISTINCT Artista.nome
---FROM Artista
---WHERE (
---SELECT Artista.nome
---FROM Artista, Utilizador JOIN UtilizadorSegueArtista
---ON Utilizador.username = UtilizadorSegueArtista.username AND Artista.artistaID=UtilizadorSegueArtista.artistaID
---WHERE Utilizador.username = 'sfisher18'
---GROUP BY Artista.nome)<> (
-
---SELECT DISTINCT Artista.nome
---FROM Artista, MusicaOuvida JOIN MusicaAlbumArtista
---ON Artista.artistaID=MusicaAlbumArtista.artistaID AND MusicaOuvida.musicaID=MusicaAlbumArtista.musicaID
---GROUP BY Artista.nome
---)
---GROUP BY Artista.nome
