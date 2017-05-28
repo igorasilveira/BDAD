ab1805292d56d83cee85d8a44cce975ad2a5b735
JMendes25 <up201505439@fe.up.pt>
2017-05-28 20:49:02 +0100

    visible

--- 3a entrega/gatilho3_verifica.sql
+++ 3a entrega/gatilho3_verifica.sql
@@ -2,11 +2,11 @@
 -- TRIGGER 3 - ADICIONA
 -- Ao seguir um utilizador, é criada uma playlist propria com as musicas favoritas do utilizador seguido
 
-select PlaylistGuardaMusica.musicaID from PlaylistGuardaMusica, Playlist
+select PlaylistGuardaMusica.musicaID AS musicaID_1 from PlaylistGuardaMusica, Playlist
 where (Playlist.criador = 'jcook11' and PlaylistGuardaMusica.playlistID = Playlist.playlistID);
 
 insert into UtilizadorSegueUtilizador (username1, username2) values ('jcook11', 'rkelleyj');
   insert into UtilizadorSegueUtilizador (username1, username2) values ('cowenso', 'jcook11');
 
-  select PlaylistGuardaMusica.musicaID from PlaylistGuardaMusica, Playlist
+  select PlaylistGuardaMusica.musicaID AS musicaID_2 from PlaylistGuardaMusica, Playlist
   where (Playlist.criador = 'jcook11' and PlaylistGuardaMusica.playlistID = Playlist.playlistID);
